d509988d60f798241aa22bc91009ac8fe59ec9db
Al-Ariqi Fadi <fadyalryqy0@gmail.com>
2024-11-02 19:56:25 +0300

    Fixed website style

--- resources/views/website/layout/master.blade.php
+++ resources/views/website/layout/master.blade.php
@@ -1,47 +1,47 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="{{app()->getLocale() == 'ar' ? 'rtl' : 'ltr'}}" class="bg-light">
+<html lang="{{app()->getLocale() == 'ar' ? : 'en'}}" dir="{{app()->getLocale() == 'ar' ? 'rtl' : 'ltr'}}">
+
 
 <head>
     <meta charset="utf-8">
-    <title>NiGC</title>
+    <title>Nigc</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
+
+
+
+
+
 
     <!-- Google Web Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600;700&family=Poppins:wght@200;300;400;500;600&display=swap" rel="stylesheet">
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.rtl.min.css" integrity="sha384-dpuaG1suU0eT09tx5plTaGMLBsfDLzUCCUXOY2j/LSvXYuG6Bqs43ALlhIqAJVRb" crossorigin="anonymous">
-
 
     <!-- Icon Font Stylesheet -->
-{{--    <link rel="stylesheet" href="{{asset('website/css/all.css')}}"/>--}}
-{{--    <link href="{{asset('website/css/bootstrap-icons.css')}}" rel="stylesheet">--}}
-
-    <!-- Icon Font Stylesheet -->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"/>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css"/>
-
+    <link rel="stylesheet" href="{{asset('website/css/all.css')}}"/>
+    <link href="{{asset('website/css/bootstrap-icons.css')}}" rel="stylesheet">
 
     <!-- Libraries Stylesheet -->
     <link rel="stylesheet" type="text/css" href="{{asset('website/lib/animate/animate.min.css')}}">
     <link rel="stylesheet" type="text/css" href="{{asset('website/lib/owlcarousel/assets/owl.carousel.min.css')}}">
 
 
+
+
     <!-- Customized Bootstrap Stylesheet -->
-    <link rel="stylesheet" type="text/css" href="{{asset('website/css/bootstrap.min.css')}}">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.rtl.min.css" integrity="sha384-dpuaG1suU0eT09tx5plTaGMLBsfDLzUCCUXOY2j/LSvXYuG6Bqs43ALlhIqAJVRb" crossorigin="anonymous">
+{{--    <link rel="stylesheet" type="text/css" href="{{asset('website/css/bootstrap.min.css')}}">--}}
+{{--    <link rel="stylesheet" type="text/css" href="{{asset('website/css/bootstrap_rtl.min.css')}}">--}}
 
     <!-- Template Stylesheet -->
-    <link rel="stylesheet" type="text/css" href="{{asset('website/css/style.css')}}">
-
-{{--    language   --}}
     @if(app()->getLocale() == 'ar')
-        <link rel="stylesheet" href="{{asset('website/css/styler_rtl.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{asset('website/css/style_rtl.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{asset('website/css/bootstrap_rtl.min.css')}}">
     @else
-        <link rel="stylesheet" href="{{asset('website/css/style.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{asset('website/css/style.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{asset('website/css/bootstrap.min.css')}}">
     @endif
 </head>
 
