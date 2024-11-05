1c0c4bdfae9d0f696650ac6372552c24c4906e89
Al-Ariqi Fadi <fadyalryqy0@gmail.com>
2024-11-05 23:24:34 +0300

    .

--- resources/views/admin/include/footer.blade.php
+++ resources/views/admin/include/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="footer">
-    &copy; <?php echo date('Y'); ?> <span class="d-none d-md-inline-block"> Devoloped by <i class="mdi mdi-heart text-danger"></i> <span style="color: #E3731D"> Flame Base</span> Team</span>
+    &copy; <?php echo date('Y'); ?> <span class="d-none d-md-inline-block"> Devoloped by <i class="mdi mdi-heart text-danger"></i> <a href="https://flamebases.com" target="_blank"><span style="color: #E3731D"> Flame Base</span></a> Team</span>
 </footer>
